11e610e6cf711a1d49b87e7db87e45188f1a8672
rcon <reg710@gmail.com>
2023-09-11 19:26:49 -0400

    listing 2.10

--- bicycles.rb
+++ bicycles.rb
@@ -21,3 +21,25 @@ end
 
 puts Gear.new(52, 11, 26, 1.5).gear_inches
 puts Gear.new(52, 11, 24, 1.25).gear_inches
+
+class RevealingReferences
+    attr_reader :wheels
+    def initialize(data)
+        @wheels = wheelify(data)
+    end
+
+    def diameters
+        wheels.collect { |wheel| 
+            wheel.rim + (wheel.tire * 2)
+        }
+    end
+
+    Wheel = Struct.new(:rim, :tire)
+    def wheelify(data)
+        data.collect {|cell| 
+            Wheel.new(cell[0], cell[1])
+        }
+    end
+end
+
+puts RevealingReferences.new([[622, 20], [622, 23]]).diameters
